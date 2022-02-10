df547991c5b0e9ff89d8e1152f1fd70f63f3e268
AAstroPhysiCS <okangueclue@hotmail.com>
2022-02-11 00:04:54 +0100

    code cleanup

--- LucyEngine/src/Renderer/Shader/Shader.cpp
+++ LucyEngine/src/Renderer/Shader/Shader.cpp
@@ -144,155 +144,85 @@ namespace Lucy {
 		spirv_cross::ShaderResources resourcesVertex = compilerVertex->get_shader_resources();
 		spirv_cross::ShaderResources resourcesFragment = compilerFragment->get_shader_resources();
 
-		m_ShaderInfoVertex.UniformCount = resourcesVertex.uniform_buffers.size();
-		m_ShaderInfoVertex.SampledImagesCount = resourcesVertex.sampled_images.size();
-		m_ShaderInfoVertex.PushConstantBufferCount = resourcesVertex.push_constant_buffers.size();
-		m_ShaderInfoVertex.StageInputCount = resourcesVertex.stage_inputs.size();
-		m_ShaderInfoVertex.StageOutputCount = resourcesVertex.stage_outputs.size();
+		auto reflect = [this](spirv_cross::Compiler* compiler, spirv_cross::ShaderResources resource, ShaderStageInfo& stageInfo) {
+			stageInfo.UniformCount = resource.uniform_buffers.size();
+			stageInfo.SampledImagesCount = resource.sampled_images.size();
+			stageInfo.PushConstantBufferCount = resource.push_constant_buffers.size();
+			stageInfo.StageInputCount = resource.stage_inputs.size();
+			stageInfo.StageOutputCount = resource.stage_outputs.size();
 
-		LUCY_INFO(fmt::format("------Vertex Shader {0}------", m_Path));
-		LUCY_INFO(fmt::format("{0} uniform buffers", m_ShaderInfoVertex.UniformCount));
-		LUCY_INFO(fmt::format("{0} sampled images", m_ShaderInfoVertex.SampledImagesCount));
-		LUCY_INFO(fmt::format("{0} push constant buffers", m_ShaderInfoVertex.PushConstantBufferCount));
-		LUCY_INFO(fmt::format("{0} stage inputs", m_ShaderInfoVertex.StageInputCount));
-		LUCY_INFO(fmt::format("{0} stage outputs", m_ShaderInfoVertex.StageOutputCount));
+			LUCY_INFO(fmt::format("------Shader {0}------", m_Path));
+			LUCY_INFO(fmt::format("{0} uniform buffers", stageInfo.UniformCount));
+			LUCY_INFO(fmt::format("{0} sampled images", stageInfo.SampledImagesCount));
+			LUCY_INFO(fmt::format("{0} push constant buffers", stageInfo.PushConstantBufferCount));
+			LUCY_INFO(fmt::format("{0} stage inputs", stageInfo.StageInputCount));
+			LUCY_INFO(fmt::format("{0} stage outputs", stageInfo.StageOutputCount));
 
-		for (const auto& ub : resourcesVertex.uniform_buffers) {
-			const auto& bufferType = compilerVertex->get_type(ub.base_type_id);
-			uint32_t set = compilerVertex->get_decoration(ub.id, spv::DecorationDescriptorSet);
-			uint32_t bufferSize = compilerVertex->get_declared_struct_size(bufferType);
-			uint32_t binding = compilerVertex->get_decoration(ub.id, spv::DecorationBinding);
-			int32_t memberCount = bufferType.member_types.size();
+			for (const auto& ub : resource.uniform_buffers) {
+				const auto& bufferType = compiler->get_type(ub.base_type_id);
+				uint32_t set = compiler->get_decoration(ub.id, spv::DecorationDescriptorSet);
+				uint32_t bufferSize = compiler->get_declared_struct_size(bufferType);
+				uint32_t binding = compiler->get_decoration(ub.id, spv::DecorationBinding);
+				int32_t memberCount = bufferType.member_types.size();
 
-			LUCY_INFO(fmt::format("{0}", ub.name));
-			LUCY_INFO(fmt::format("Size = {0}", bufferSize));
-			LUCY_INFO(fmt::format("Binding = {0}", binding));
-			LUCY_INFO(fmt::format("Set = {0}", set));
-			LUCY_INFO(fmt::format("Members = {0}", memberCount));
+				LUCY_INFO(fmt::format("{0}", ub.name));
+				LUCY_INFO(fmt::format("Size = {0}", bufferSize));
+				LUCY_INFO(fmt::format("Binding = {0}", binding));
+				LUCY_INFO(fmt::format("Set = {0}", set));
+				LUCY_INFO(fmt::format("Members = {0}", memberCount));
 
-			ShaderUniformBufferInfo info;
-			info.Set = set;
-			info.Binding = binding;
-			info.BufferSize = bufferSize;
-			info.MemberCount = memberCount;
-			info.Type = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
-			info.Name = ub.name.c_str();
-			info.StageFlag = VK_SHADER_STAGE_VERTEX_BIT;
+				ShaderUniformBufferInfo info;
+				info.Set = set;
+				info.Binding = binding;
+				info.BufferSize = bufferSize;
+				info.MemberCount = memberCount;
+				info.Type = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
+				info.Name = ub.name.c_str();
+				info.StageFlag = VK_SHADER_STAGE_VERTEX_BIT;
 
-			if (!m_DescriptorSetMap.count(set)) {
-				std::vector<ShaderUniformBufferInfo> buffer;
-				buffer.push_back(info);
-				m_DescriptorSetMap.emplace(set, buffer);
-			} else {
-				auto& it = m_DescriptorSetMap.find(set);
-				it->second.push_back(info);
+				if (!m_DescriptorSetMap.count(set)) {
+					std::vector<ShaderUniformBufferInfo> buffer;
+					buffer.push_back(info);
+					m_DescriptorSetMap.emplace(set, buffer);
+				} else {
+					auto& it = m_DescriptorSetMap.find(set);
+					it->second.push_back(info);
+				}
 			}
-		}
 
-		for (const auto& ui : resourcesVertex.sampled_images) {
-			const auto& bufferType = compilerVertex->get_type(ui.base_type_id);
-			uint32_t set = compilerVertex->get_decoration(ui.id, spv::DecorationDescriptorSet);
-			uint32_t binding = compilerVertex->get_decoration(ui.id, spv::DecorationBinding);
-			int32_t memberCount = bufferType.member_types.size();
+			for (const auto& ui : resource.sampled_images) {
+				const auto& bufferType = compiler->get_type(ui.base_type_id);
+				uint32_t set = compiler->get_decoration(ui.id, spv::DecorationDescriptorSet);
+				uint32_t binding = compiler->get_decoration(ui.id, spv::DecorationBinding);
+				int32_t memberCount = bufferType.member_types.size();
 
-			LUCY_INFO(fmt::format("{0}", ui.name));
-			LUCY_INFO(fmt::format("Set = {0}", set));
-			LUCY_INFO(fmt::format("Binding = {0}", binding));
-			LUCY_INFO(fmt::format("Members = {0}", memberCount));
+				LUCY_INFO(fmt::format("{0}", ui.name));
+				LUCY_INFO(fmt::format("Set = {0}", set));
+				LUCY_INFO(fmt::format("Binding = {0}", binding));
+				LUCY_INFO(fmt::format("Members = {0}", memberCount));
 
-			ShaderUniformBufferInfo info;
-			info.Set = set;
-			info.Binding = binding;
-			info.BufferSize = 0;
-			info.MemberCount = memberCount;
-			info.Type = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
-			info.Name = ui.name.c_str();
-			info.StageFlag = VK_SHADER_STAGE_VERTEX_BIT;
+				ShaderUniformBufferInfo info;
+				info.Set = set;
+				info.Binding = binding;
+				info.BufferSize = 0;
+				info.MemberCount = memberCount;
+				info.Type = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
+				info.Name = ui.name.c_str();
+				info.StageFlag = VK_SHADER_STAGE_VERTEX_BIT;
 
-			if (!m_DescriptorSetMap.count(set)) {
-				std::vector<ShaderUniformBufferInfo> buffer;
-				buffer.push_back(info);
-				m_DescriptorSetMap.emplace(set, buffer);
-			} else {
-				auto& it = m_DescriptorSetMap.find(set);
-				it->second.push_back(info);
+				if (!m_DescriptorSetMap.count(set)) {
+					std::vector<ShaderUniformBufferInfo> buffer;
+					buffer.push_back(info);
+					m_DescriptorSetMap.emplace(set, buffer);
+				} else {
+					auto& it = m_DescriptorSetMap.find(set);
+					it->second.push_back(info);
+				}
 			}
-		}
+		};
 
-		m_ShaderInfoFragment.UniformCount = resourcesFragment.uniform_buffers.size();
-		m_ShaderInfoFragment.SampledImagesCount = resourcesFragment.sampled_images.size();
-		m_ShaderInfoFragment.PushConstantBufferCount = resourcesFragment.push_constant_buffers.size();
-		m_ShaderInfoFragment.StageInputCount = resourcesFragment.stage_inputs.size();
-		m_ShaderInfoFragment.StageOutputCount = resourcesFragment.stage_outputs.size();
-
-		LUCY_INFO(fmt::format("------Fragment Shader {0}------", m_Path));
-		LUCY_INFO(fmt::format("{0} uniform buffers", m_ShaderInfoFragment.UniformCount));
-		LUCY_INFO(fmt::format("{0} sampled images", m_ShaderInfoFragment.SampledImagesCount));
-		LUCY_INFO(fmt::format("{0} push constant buffers", m_ShaderInfoFragment.PushConstantBufferCount));
-		LUCY_INFO(fmt::format("{0} stage inputs", m_ShaderInfoFragment.StageInputCount));
-		LUCY_INFO(fmt::format("{0} stage outputs", m_ShaderInfoFragment.StageOutputCount));
-
-		for (const auto& ub : resourcesFragment.uniform_buffers) {
-			const auto& bufferType = compilerFragment->get_type(ub.base_type_id);
-			uint32_t set = compilerFragment->get_decoration(ub.id, spv::DecorationDescriptorSet);
-			uint32_t bufferSize = compilerFragment->get_declared_struct_size(bufferType);
-			uint32_t binding = compilerFragment->get_decoration(ub.id, spv::DecorationBinding);
-			int32_t memberCount = bufferType.member_types.size();
-
-			LUCY_INFO(fmt::format("{0}", ub.name));
-			LUCY_INFO(fmt::format("Set = {0}", set));
-			LUCY_INFO(fmt::format("Size = {0}", bufferSize));
-			LUCY_INFO(fmt::format("Binding = {0}", binding));
-			LUCY_INFO(fmt::format("Members = {0}", memberCount));
-
-			ShaderUniformBufferInfo info;
-			info.Set = set;
-			info.Binding = binding;
-			info.BufferSize = bufferSize;
-			info.MemberCount = memberCount;
-			info.Type = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
-			info.Name = ub.name.c_str();
-			info.StageFlag = VK_SHADER_STAGE_FRAGMENT_BIT;
-
-			if (!m_DescriptorSetMap.count(set)) {
-				std::vector<ShaderUniformBufferInfo> buffer;
-				buffer.push_back(info);
-				m_DescriptorSetMap.emplace(set, buffer);
-			} else {
-				auto& it = m_DescriptorSetMap.find(set);
-				it->second.push_back(info);
-			}
-		}
-
-		for (const auto& ui : resourcesFragment.sampled_images) {
-			const auto& bufferType = compilerFragment->get_type(ui.base_type_id);
-			uint32_t set = compilerFragment->get_decoration(ui.id, spv::DecorationDescriptorSet);
-			uint32_t binding = compilerFragment->get_decoration(ui.id, spv::DecorationBinding);
-			int32_t memberCount = bufferType.member_types.size();
-
-			LUCY_INFO(fmt::format("{0}", ui.name));
-			LUCY_INFO(fmt::format("Set = {0}", set));
-			LUCY_INFO(fmt::format("Binding = {0}", binding));
-			LUCY_INFO(fmt::format("Members = {0}", memberCount));
-
-			ShaderUniformBufferInfo info;
-			info.Set = set;
-			info.Binding = binding;
-			info.BufferSize = 0;
-			info.MemberCount = memberCount;
-			info.Type = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
-			info.Name = ui.name.c_str();
-			info.StageFlag = VK_SHADER_STAGE_FRAGMENT_BIT;
-
-			if (!m_DescriptorSetMap.count(set)) {
-				std::vector<ShaderUniformBufferInfo> buffer;
-				buffer.push_back(info);
-				m_DescriptorSetMap.emplace(set, buffer);
-			} else {
-				auto& it = m_DescriptorSetMap.find(set);
-				it->second.push_back(info);
-			}
-		}
+		reflect(compilerVertex, resourcesVertex, m_ShaderInfoVertex);
+		reflect(compilerFragment, resourcesFragment, m_ShaderInfoFragment);
 
 		delete compilerVertex;
 		delete compilerFragment;
